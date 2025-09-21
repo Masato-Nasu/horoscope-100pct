7995b848cf21138f28f73c5f7a7e45bba739717d
Masato-Nasu <1bitexist@gmail.com>
2025-09-21 17:30:59 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "hoshi-cache-v3";
+const CACHE_NAME = "hoshi-cache-v4";
 const URLS_TO_CACHE = [
   "./",
   "./index.html",
@@ -22,5 +22,6 @@ self.addEventListener("fetch", event => {
     })
   );
 });
+
 
 

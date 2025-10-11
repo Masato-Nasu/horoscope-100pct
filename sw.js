2ff7ae470c7e2ff4b12ebadd89b116a7fbba2aa6
Masato-Nasu <1bitexist@gmail.com>
2025-10-11 14:32:42 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,29 +1,7 @@
-/* sw.js v38 */
-const CACHE_VERSION = 'v38';
-const APP_CACHE = 'horoscope-' + CACHE_VERSION;
-const ASSETS = [
-  './',
-  './index.html?v=38',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
-];
-
-self.addEventListener('install', e=>{
-  e.waitUntil(caches.open(APP_CACHE).then(c=>c.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e=>{
-  e.waitUntil((async()=>{
-    const keys=await caches.keys();
-    await Promise.all(keys.filter(k=>k!==APP_CACHE).map(k=>caches.delete(k)));
-    await self.clients.claim();
-  })());
-});
-
-self.addEventListener('fetch', e=>{
-  e.respondWith(
-    caches.match(e.request).then(r=>r || fetch(e.request))
-  );
-});
+/* sw.js v40 */
+const CACHE_VERSION='v40';
+const APP_CACHE='horoscope-'+CACHE_VERSION;
+const ASSETS=['./','./index.html?v=40','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(APP_CACHE).then(c=>c.addAll(ASSETS))); self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil((async()=>{const ks=await caches.keys(); await Promise.all(ks.filter(k=>k!==APP_CACHE).map(k=>caches.delete(k))); await self.clients.claim();})());});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});

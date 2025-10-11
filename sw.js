f3efad163b91fa3146212ffecafde0f1770525ae
Masato-Nasu <1bitexist@gmail.com>
2025-10-11 16:42:28 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,19 @@
-/* sw.js v46 */
-const CACHE_VERSION='v46';
+/* sw.js v47 */
+const CACHE_VERSION='v47';
 const APP_CACHE='horoscope-'+CACHE_VERSION;
-const ASSETS=['./','./index.html?v=46','./manifest.json','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(APP_CACHE).then(c=>c.addAll(ASSETS))); self.skipWaiting();});
-self.addEventListener('activate',e=>{e.waitUntil((async()=>{const ks=await caches.keys(); await Promise.all(ks.filter(k=>k!==APP_CACHE).map(k=>caches.delete(k))); await self.clients.claim();})());});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
+const ASSETS=['./','./index.html?v=47','./manifest.json','./icon-192.png','./icon-512.png'];
+
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(APP_CACHE).then(c=>c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil((async()=>{
+    const ks=await caches.keys();
+    await Promise.all(ks.filter(k=>k!==APP_CACHE).map(k=>caches.delete(k)));
+    await self.clients.claim();
+  })());
+});
+self.addEventListener('fetch',e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});

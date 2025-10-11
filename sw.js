92bec81ed4d7ce2e5adb23a6052d3a350337a04f
Masato-Nasu <1bitexist@gmail.com>
2025-10-11 15:12:43 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-/* sw.js v41 */
-const CACHE_VERSION='v41';
+/* sw.js v42 */
+const CACHE_VERSION='v42';
 const APP_CACHE='horoscope-'+CACHE_VERSION;
-const ASSETS=['./','./index.html?v=41','./manifest.json','./icon-192.png','./icon-512.png'];
+const ASSETS=['./','./index.html?v=42','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(APP_CACHE).then(c=>c.addAll(ASSETS))); self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil((async()=>{const ks=await caches.keys(); await Promise.all(ks.filter(k=>k!==APP_CACHE).map(k=>caches.delete(k))); await self.clients.claim();})());});
 self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
